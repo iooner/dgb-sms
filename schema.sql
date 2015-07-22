c90655ea5966871a90ccf6696fff8b8a1c8b74c0
Maxime Daniel <root@maxux.net>
2015-07-22 16:58:51 +0200

    sql schema updated

--- schema.sql
+++ schema.sql
@@ -1,3 +1,7 @@
+CREATE TABLE raw (id integer primary key, arrived datetime, pdu text);
 CREATE TABLE messages (id integer primary key, number varchar(32), message varchar(2048), received datetime, read int);
 CREATE TABLE pending (id integer primary key, number varchar(32), message varchar(1024), sent integer default 0, type integer default 0, added datetime default CURRENT_TIMESTAMP, departure datetime default NULL);
 CREATE TABLE failed (id integer primary key, pdu varchar(2048), added datetime default current_timestamp);
+CREATE TABLE segments (id integer primary key, number varchar(32), message varchar(2048), received datetime, partid integer, part integer, total integer);
+CREATE TABLE bnv (id integer primary key, prenom text, nom text, number text, pause text);
+
